141923d28576239d51e904568d64ab12aa213a30
nedoratic <nedoratic@gmail.com>
2023-12-05 13:40:12 +0100

    functional programming

---
+++ 020-combine-an-array-into-a-string-using-the-join-method/script.js
@@ -0,0 +1,17 @@
+// The join method is used to join the elements of an array together to create a string. It takes an argument for the delimiter that is used to separate the array elements in the string.
+
+// Here's an example:
+
+const arr = ["Hello", "World"];
+
+const str = arr.join(" ");
+
+// str would have a value of the string Hello World.
+
+// TASK
+
+// Use the join method (among others) inside the sentensify function to make a sentence from the words in the string str. The function should return a string. For example, I-like-Star-Wars would be converted to I like Star Wars. For this challenge, do not use the replace method.
+
+const sentensify = (str) => str.split(/\W+/).join(" ");
+
+console.log(sentensify("May-the-force-be-with-you"));
